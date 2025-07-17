5663378410a5d61df412d784e89de929e2a5329c
user <ersh0ff.il@yandex.ru>
2025-07-17 14:07:32 +0300

    Update 1

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,47 @@
 public class Employee {
+    private static int idCreate = 1;
 
+    private final int id;
+    private final String fullName;
+    private int department;
+    private long salary;
+
+    public Employee(String fullName, int department, int salary) {
+        id = idCreate++;
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary;
+    }
 }
